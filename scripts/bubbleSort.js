48880c44786361bc01696763130e69d200df48f9
Edilene <edilene_cruz@yahoo.com.br>
2017-04-29 17:53:52 -0600

    bubbleSort without test

---
+++ scripts/bubbleSort.js
@@ -0,0 +1,24 @@
+var input = ["d", "b", "a", "c"];
+var input2 = [ 5, 50, 25, 500, 15, 750]
+
+function bubbleSort (input) {
+  var swapped = true;
+  while(swapped) {
+    swapped = false;
+  for (var i = 0; i < input.length - 1; i++) {
+    if (input[i] > input[i + 1]) {
+        var temp = input[i];
+        input[i] = input[i+1];
+        input[i+1] = temp;
+        swapped = true;
+      }
+    }
+  }
+}
+
+
+bubbleSort(input);
+console.log(input);
+
+bubbleSort(input2);
+console.log(input2);
